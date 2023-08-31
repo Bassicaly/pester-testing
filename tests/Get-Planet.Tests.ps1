b082376d6c8d49946f6e37742cf745195b10a1b2
Bas de Bolster <bas.de.bolster@achmea.nl>
2023-08-31 14:57:03 +0200

    breaking tests

--- tests/Get-Planet.Tests.ps1
+++ tests/Get-Planet.Tests.ps1
@@ -9,6 +9,7 @@ BeforeAll {
       @{ Name = 'Saturn' }
       @{ Name = 'Uranus' }
       @{ Name = 'Neptune' }
+      @{ Name = 'Pluto' }
     ) | ForEach-Object { [PSCustomObject] $_ }
 
     $planets | Where-Object { $_.Name -like $Name }
@@ -19,5 +20,22 @@ Describe 'Get-Planet' {
   It 'Given no parameters, it lists all 8 planets' {
     $allPlanets = Get-Planet
     $allPlanets.Count | Should -Be 8
+  }
+
+  It 'Earth is the third planet in our Solar System' {
+    $allPlanets = Get-Planet
+    $allPlanets[2].Name | Should -Be 'Earth'
+  }
+
+  It 'Pluto is not part of our Solar System' {
+    $allPlanets = Get-Planet
+    $plutos = $allPlanets | Where-Object Name -eq 'Pluto'
+    $plutos.Count | Should -Be 0
+  }
+
+  It 'Planets have this order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune' {
+    $allPlanets = Get-Planet
+    $planetsInOrder = $allPlanets.Name -join ', '
+    $planetsInOrder | Should -Be 'Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune'
   }
 }
